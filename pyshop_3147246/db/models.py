aa7530389d1d581e98c0636644e224def2525a12
diegoarm18 <rodriguezdiegoandres782@gmail.com>
2025-05-27 11:01:13 -0500

    cineccion base de datos con python

--- pyshop_3147246/db/models.py
+++ pyshop_3147246/db/models.py
@@ -1,11 +1,17 @@
 from .database import Base
-from sqlalchemy import column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 #Crear la base del modelo (Entidad)
 class Categoria(Base):
     __tablename__= "categorias"
-    id = column(Integer, 
-                PrimaryKey = True
-                )
-    nombre = column(String(50))
-    
+    id = Column(Integer, primary_key = True,)
+    nombre = Column(String(50))
+
+class Productos(Base):
+    __tablename__ = "productos"
+    nombre = Column(String(40))
+    modelo=Column(String(60))
+    precio=Column(Integer)
+    id=Column(Integer, 
+              primary_key=True)
+    Categoria_id = Column(Integer, ForeignKey("categorias.id"))
